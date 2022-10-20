7d99ea69bead78410404fdf968bbefe1d53c72b4
rose <47357290+rose4096@users.noreply.github.com>
2022-10-20 17:35:22 -0400

    Add more examples add support for breaking versions, clean code, fix solidity grammar

--- setup.sh
+++ setup.sh
@@ -8,5 +8,7 @@ if [[ "$VIRTUAL_ENV" == "" ]]; then
   python3 -m pip install -r requirements.txt
 else
   echo "Looks like youre already in a venv :)"
-fi
-java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 Solidity.g4
+\fi
+java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 grammar/v060/Solidity.g4 -o grammar/v060/
+java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 grammar/v070/Solidity.g4 -o grammar/v070/
+java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 grammar/v080/SolidityParser.g4 grammar/v080/SolidityLexer.g4 -o grammar/v080/
---
+++ src/collectors/v070.py
@@ -0,0 +1,49 @@
+from antlr4 import InputStream, CommonTokenStream, TerminalNode
+
+from grammar.v070.SolidityLexer import SolidityLexer
+from grammar.v070.SolidityParser import SolidityParser
+from src.collectors.v000 import TopLevelObjectCollector, TopLevelObject
+
+
+class TopLevelObjectCollectorV070(TopLevelObjectCollector):
+    TOP_LEVEL_OBJECT_TYPES = [SolidityParser.StructDefinitionContext, SolidityParser.EnumDefinitionContext,
+                              SolidityParser.ContractDefinitionContext]
+
+    def __init__(self):
+        super().__init__()
+
+    def visit(self, node, parent=None):
+        if isinstance(node, TerminalNode):
+            self.visit_terminal_node(node, parent)
+        elif node.children is None:
+            return ''
+        elif isinstance(node, SolidityParser.StatementContext):
+            assert len(node.children) == 1
+            self.visit(node.children[0], node)
+            self.newline()
+        elif isinstance(node, SolidityParser.ContractPartContext):
+            assert len(node.children) == 1
+            self.visit(node.children[0], node)
+            self.cur_line += '\n'
+            self.newline()
+        else:
+            for c in node.children:
+                self.visit(c, node)
+
+    def collect(self, stream):
+        data = InputStream(stream)
+        lexer = SolidityLexer(data)
+        stream = CommonTokenStream(lexer)
+        parser = SolidityParser(stream)
+
+        tree = parser.sourceUnit()
+        source_units = tree.children
+
+        result = []
+        for su in source_units:
+            if type(su) in TopLevelObjectCollectorV070.TOP_LEVEL_OBJECT_TYPES:
+                self.reset()
+                self.visit(su)
+                result.append(TopLevelObject(su.identifier().Identifier(), self.collect_lines()))
+
+        return result
