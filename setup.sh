764b74d55dd0dc42a78d3450374a4438646684a8
Stephen Tong <14918218+stong@users.noreply.github.com>
2022-10-21 04:56:50 +0100

    Bruh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ if [[ "$VIRTUAL_ENV" == "" ]]; then
   python3 -m pip install -r requirements.txt
 else
   echo "Looks like youre already in a venv :)"
-\fi
+fi
 java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 solidity_parser/grammar/v060/Solidity.g4 -o solidity_parser/grammar/v060/
 java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 solidity_parser/grammar/v070/Solidity.g4 -o solidity_parser/grammar/v070/
 java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 solidity_parser/grammar/v080/SolidityParser.g4 solidity_parser/grammar/v080/SolidityLexer.g4 -o solidity_parser/grammar/v080/
