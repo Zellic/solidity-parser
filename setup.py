2b5215d931d6897a3d576df402a9458863164caf
Bilal Tariq <biblthebest@hotmail.com>
2023-04-17 00:47:49 +0100

    make setup.py generate grammar files

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import setup, find_packages
+import subprocess
+
+
+cmds = [
+    'java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 src/solidity_parser/grammar/v060/Solidity.g4 -o src/solidity_parser/grammar/v060/',
+    'java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 src/solidity_parser/grammar/v070/Solidity.g4 -o src/solidity_parser/grammar/v070/',
+    'java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 src/solidity_parser/grammar/v080/SolidityParser.g4 src/solidity_parser/grammar/v080/SolidityLexer.g4 -o src/solidity_parser/grammar/v080/',
+    'java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 src/solidity_parser/grammar/v088/SolidityParser.g4 src/solidity_parser/grammar/v088/SolidityLexer.g4 -o src/solidity_parser/grammar/v088/'
+]
+
+print("Generating antlr parsers")
+
+for c in cmds:
+    subprocess.run(c.split(' '), check=True)
+
+print("Generated antlr parsers")
 
 setup(
     name='solidity-parser',
