8ecd7bf9f74d08dc142143cc4d32692aa0143309
Bilal Tariq <biblthebest@hotmail.com>
2023-11-08 14:57:19 +0000

    fix setup for 0.8.2

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ class BuildPyWithGenerateCommand(setuptools.command.build_py.build_py):
             'java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 src/solidity_parser/grammar/v060/Solidity.g4 -o src/solidity_parser/grammar/v060/ -Xexact-output-dir',
             'java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 src/solidity_parser/grammar/v070/Solidity.g4 -o src/solidity_parser/grammar/v070/ -Xexact-output-dir',
             'java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 src/solidity_parser/grammar/v080/SolidityParser.g4 src/solidity_parser/grammar/v080/SolidityLexer.g4 -o src/solidity_parser/grammar/v080/ -Xexact-output-dir',
-            'java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 src/solidity_parser/grammar/v088/SolidityParser.g4 src/solidity_parser/grammar/v088/SolidityLexer.g4 -o src/solidity_parser/grammar/v088/ -Xexact-output-dir' ]
+            'java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 src/solidity_parser/grammar/v088/SolidityParser.g4 src/solidity_parser/grammar/v088/SolidityLexer.g4 -o src/solidity_parser/grammar/v088/ -Xexact-output-dir',
+            'java -jar vendor/antlr-4.11.1-complete.jar -Dlanguage=Python3 src/solidity_parser/grammar/v08_22/SolidityParser.g4 src/solidity_parser/grammar/v08_22/SolidityLexer.g4 -o src/solidity_parser/grammar/v08_22/ -Xexact-output-dir']
 
         print("Generating antlr parsers")
 
