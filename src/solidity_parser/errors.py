c6005bb3cb7ab95b4f369a28d736f8d7df5e04b5
Bilal Tariq <bilal@zellic.io>
2023-12-19 16:15:38 +0000

    remove commented code

--- src/solidity_parser/errors.py
+++ src/solidity_parser/errors.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 
 from solidity_parser.util.version_util import Version
 
+
 class AntlrParsingError(Exception):
     Detail = namedtuple('Detail', 'line_number, line_offset, msg')
 
@@ -16,23 +17,3 @@ class CodeProcessingError(Exception):
     def __init__(self, message, source_unit_name, line_number, line_offset):
         super().__init__(f'{source_unit_name}@{line_number}:{line_offset} > {message}')
 
-# class SolParserAssertionError(Exception):
-#     pass
-
-
-# def not_implemented(*args):
-#     raise SolParserAssertionError(*args)
-#
-#
-# def fail_assumption(*args):
-#     raise SolParserAssertionError(*args)
-#
-#
-# def assume(predicate, msg=None):
-#     try:
-#         if msg:
-#             assert predicate, msg
-#         else:
-#             assert predicate
-#     except AssertionError as e:
-#         raise SolParserAssertionError(e)
