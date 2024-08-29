6b085459baf91ce8f806fe6eb114a4974f9c802c
Bilal Tariq <bilal@zellic.io>
2024-08-29 11:19:16 +0100

    0.1.22 release

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class BuildPyWithGenerateCommand(setuptools.command.build_py.build_py):
 
 setup(
     name='solidity-parser',
-    version='0.1.21',
+    version='0.1.22',
 
     install_requires=[
         "antlr4-python3-runtime==4.11.1",
