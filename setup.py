6eaecce64b9fb6341b6fc784f3b30b161c984b3d
Bilal <bilal@zellic.io>
2025-05-19 18:10:22 +0100

    bump version to 0.1.23

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class BuildPyWithGenerateCommand(setuptools.command.build_py.build_py):
 
 setup(
     name='solidity-parser',
-    version='0.1.22',
+    version='0.1.23',
 
     install_requires=[
         "antlr4-python3-runtime==4.11.1",
